654697c9821f337d3e1de1a33a008e5d6182476a
Kevni Woodside <96272858+Code-With-Kev@users.noreply.github.com>
2022-04-10 14:01:31 -0700

    Initial Commit

---
+++ Casino-Game/Casino_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Casino_GameApp.swift
+//  Casino-Game
+//
+//  Created by Kevni Woodside on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Casino_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
